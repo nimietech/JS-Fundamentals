fed7e954fe4863215c319eda1ae94adb93416aa9
Glory <gloryomoruyi00@gmail.com>
2025-11-14 11:14:58 +0100

    Made a little correction

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,10 @@
 
-const times = parseInt(process.argv[2]);
+const x = parseInt(process.argv[2]);
 
-if (isNaN(times) || times <= 0) {
+if (isNaN(x)) {
   console.log("Missing number of occurrences");
 } else {
-  for (let i = 0; i < times; i++) {
+  for (let i = 0; i < x; i++) {
     console.log("C is fun");
   }
 }
